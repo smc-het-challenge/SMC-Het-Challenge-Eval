04df33b84ab6f3a8f3bd121a035e4a087f2f4ffc
Kyle Ellrott <kellrott@gmail.com>
2016-09-26 12:32:26 -0700

    Letting results be over-written

--- eval_entry_tumor.sh
+++ eval_entry_tumor.sh
@@ -41,7 +41,7 @@ for a in tumors/$TUMOR/$TUMOR.mutect.vcf; do
     mkdir -p output/$ENTRY/$name
   fi
   bash ./run_eval_entry_tumor.sh entries/$ENTRY/repack/ $b output/$ENTRY/$name
-  gsutil cp -n -r output/* gs://smc-het-evaluation/outputs/
+  gsutil cp -r output/* gs://smc-het-evaluation/outputs/
 done
 
 
