9a68cae80045ef5c51948df38204dd1e8e4fc4ab
Kyle Ellrott <kellrott@gmail.com>
2019-04-18 09:56:05 -0700

    Fixing print for python3

--- manage.py
+++ manage.py
@@ -15,12 +15,12 @@ def clean_str(s):
 
 def command_list(syn, args):
     evaluation = syn.getEvaluation(EVALUATION_QUEUE_ID)
-    print '\n\nSubmissions for: %s %s' % (evaluation.id, evaluation.name.encode('utf-8'))
-    print '-' * 60
+    print('\n\nSubmissions for: %s %s' % (evaluation.id, evaluation.name.encode('utf-8')))
+    print('-' * 60)
     format_str = "%-8s %-8s %-30s %-10s %-10s %-30s %-30s %-20s %-20s"
     if args.tab:
         format_str = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"
-    print format_str % (
+    print(format_str % (
         "EntryID",
         "Team ID",
         "Date",
@@ -30,12 +30,12 @@ def command_list(syn, args):
         "Email",
         "Name",
         "Organisation"
-    )
+    ))
     for submission, status in syn.getSubmissionBundles(evaluation):
         team = syn.getTeam(submission.teamId)
         user = syn.getUserProfile(submission.userId)
         s = syn.getSubmission(submission.id)
-        print format_str % (
+        print( format_str % (
             submission.id,
             clean_str(team.name),
             submission.createdOn,
@@ -45,25 +45,25 @@ def command_list(syn, args):
             "%s@synapse.org" % (user['userName']),
             clean_str("%s %s" % (user['firstName'], user['lastName'])),
             clean_str(user.get('company', ''))
-        )
+        ) )
         #print submission.entity
         #print s.entity
-        
+
 def command_download(syn, args):
     if not os.path.exists(args.out):
         os.mkdir(args.out)
     for i in args.ids:
 
-        print "entry", i
+        print( "entry", i )
         entry_dir = os.path.join(args.out, i)
         if not os.path.exists(entry_dir):
             os.mkdir(entry_dir)
         sub = syn.getSubmission(i)
         for ents in ['image_entities', 'tool_entities', 'workflow_entity']:
             for ent_id in sub.entity.annotations[ents]:
-                print "downloading", ent_id
+                print( "downloading", ent_id )
                 ent = syn.get(ent_id)
-                print ent.path
+                print( ent.path )
                 if not args.cache_only:
                     shutil.copy(ent.path, os.path.join(entry_dir, os.path.basename(ent.path)))
 
@@ -79,16 +79,16 @@ def command_missing(syn, args):
                 except:
                     missing.append( (submission.id, ent_id) )
     for subid, ent in missing:
-        print subid, ent
+        print( subid, ent )
 
-                    
+
 def command_info(syn, args):
     sub = syn.getSubmission(args.id)
-    print sub.entity
+    print( sub.entity )
 
 def command_delete(syn, args):
     sub = syn.getSubmission(args.id)
-    print "Deleting"
+    print( "Deleting" )
     syn.delete(sub)
 
 def command_deployinfo(syn, args):
@@ -96,13 +96,13 @@ def command_deployinfo(syn, args):
     evaluation = syn.getEvaluation(EVALUATION_QUEUE_ID)
     for submission, status in syn.getSubmissionBundles(evaluation):
         sub = syn.getSubmission(submission.id)
-        print sub.id, sub.entity.instance_type
+        print( sub.id, sub.entity.instance_type )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     subparsers = parser.add_subparsers(title="subcommand")
-    
+
     parser_list = subparsers.add_parser('list', help="List submissions to an evaluation or list evaluations")
     parser_list.add_argument("-t", dest="tab", action="store_true", default=False)
     parser_list.set_defaults(func=command_list)
